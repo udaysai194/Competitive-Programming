448a2121918be16998357ae9d4a4dd114b121a1e
UDAYSAI VEMULA <33548299+udaysai194@users.noreply.github.com>
2022-12-15 16:34:50 +0000

    codefroces round 838 contest ends

---
+++ DefaultTemplate.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int solve() {
+
+
+}
+
+int main() {
+	int t;
+	cin >> t;
+
+	while(t--) {
+
+		cout << solve() << endl;
+
+	}
+
+	return 0;
+}
--- codeforces/Codeforces Round #838 (Div. 2)/A. Divide and Conquer/A_Divide_and_Conquer.cpp
+++ codeforces/Codeforces Round #838 (Div. 2)/A. Divide and Conquer/A_Divide_and_Conquer.cpp
@@ -3,6 +3,35 @@ using namespace std;
 
 
 int solve() {
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for(int i = 0; i < n; i++)	cin >> v[i];
+
+	long int sum = accumulate(v.begin(), v.end(), 0);
+
+	if(sum % 2 == 0)	return 0;
+
+	int minCount = INT_MAX;
+	for(auto el : v) {
+		int count = 0;
+		if(el & 1) {
+			while(el != 0 && el%2 != 0) {
+				count++;
+				el = el/2;
+			}
+			
+		} else {
+			while(el != 0 && el%2 == 0) {
+				count++;
+				el = el/2;
+			}
+		}
+		minCount = min(minCount, count);
+	}
+
+	return minCount;
+
 }
 
 int main() {
