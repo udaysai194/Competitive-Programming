5c10495e4b8de13f201ad55ec7cb2233161d449c
UDAYSAI VEMULA <33548299+udaysai194@users.noreply.github.com>
2022-12-15 14:01:19 +0000

    created test cases

--- codeforces/Educational Codeforces Round 139 (Rated for Div. 2)/B. Notepad/B_Notepad.cpp
+++ codeforces/Educational Codeforces Round 139 (Rated for Div. 2)/B. Notepad/B_Notepad.cpp
@@ -1,7 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+int solve() {
+	
+	string s;
+	cin >> s;
+	return (s.size()-1) * 9 + (s[0] - '0');
+
+}
+
 int main() {
-  
-  return 0;
+	int t;
+	cin >> t;
+
+	while(t--) {
+
+		cout << solve() << endl;
+
+	}
+
+	return 0;
 }
